e0df6ba8686834474e2bfecd9d62a3c9f06a6d4d
ZyrM <tduck.contact@gmail.com>
2024-05-31 04:22:43 +0700

    Add project files.

---
+++ DomainAliveChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainAliveChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập mail tại file combo.txt sau đó nhấn Enter để tiếp tục !!");
+            Console.Write("Nhập số luồng chạy : ");
+            int threads = Convert.ToInt32(Console.ReadLine());
+        }
+        private void InitChecking()
+        {
+
+        }
+    }
+}
